13606afb23719f2776f994e69f17c940eddcc7eb
Simon Cropp <simon.cropp@gmail.com>
2019-11-25 12:56:19 +1100

    make LogMessage public

--- src/Verify.NServiceBus/Logging/LogMessage.cs
+++ src/Verify.NServiceBus/Logging/LogMessage.cs
@@ -2,38 +2,42 @@
 using System.Collections.Generic;
 using NServiceBus.Logging;
 
-class LogMessage
+namespace Verify.NServiceBus
 {
-    public LogMessage(LogLevel level, string message, Exception? exception = null)
+    public class LogMessage
     {
-        Guard.AgainstNull(message, nameof(message));
-        Level = level;
-        Message = message;
-        Exception = exception;
-        Args = Array.Empty<object>();
-    }
-
-    public LogMessage(LogLevel level, string format, IReadOnlyList<object> args, Exception? exception = null)
-    {
-        Guard.AgainstNull(format, nameof(format));
-        Guard.AgainstNull(args, nameof(args));
-        Level = level;
-        Format = format;
-        Exception = exception;
-        try
+        public LogMessage(LogLevel level, string message, Exception? exception = null)
         {
-            Message = string.Format(format, args);
+            Guard.AgainstNull(message, nameof(message));
+            Level = level;
+            Message = message;
+            Exception = exception;
+            Args = Array.Empty<object>();
         }
-        catch (Exception formatException)
-        {
-            throw new Exception($"Could not format message. Format: {format}.", formatException);
-        }
-        Args = args;
-    }
 
-    public readonly LogLevel Level;
-    public readonly string Message;
-    public readonly Exception? Exception;
-    public readonly string? Format;
-    public readonly IReadOnlyList<object> Args;
+        public LogMessage(LogLevel level, string format, IReadOnlyList<object> args, Exception? exception = null)
+        {
+            Guard.AgainstNull(format, nameof(format));
+            Guard.AgainstNull(args, nameof(args));
+            Level = level;
+            Format = format;
+            Exception = exception;
+            try
+            {
+                Message = string.Format(format, args);
+            }
+            catch (Exception formatException)
+            {
+                throw new Exception($"Could not format message. Format: {format}.", formatException);
+            }
+
+            Args = args;
+        }
+
+        public LogLevel Level { get; }
+        public string Message { get; }
+        public Exception? Exception { get; }
+        public string? Format { get; }
+        public IReadOnlyList<object> Args { get; }
+    }
 }
