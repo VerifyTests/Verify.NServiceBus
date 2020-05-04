7151e0673e9609b9bb4a652201811dc37edb2303
SimonCropp <simon.cropp@gmail.com>
2020-05-04 14:45:57 +1000

    .

---
+++ src/Tests/MessageToHandlerMapTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using Verify.NServiceBus;
+using VerifyXunit;
+using Xunit;
+using Xunit.Abstractions;
+
+public class MessageToHandlerMapTests :
+    VerifyBase
+{
+    [Fact]
+    public Task Integration()
+    {
+        var map = new MessageToHandlerMap();
+        map.AddMessagesFromAssembly<MyMessage>();
+        map.AddHandlersFromAssembly<MyMessage>();
+        return base.Verify(map);
+    }
+
+    [Fact]
+    public Task AddMessage_type()
+    {
+        var map = new MessageToHandlerMap();
+        map.AddMessage<MyMessage>();
+        return Verify(map);
+    }
+
+    [Fact]
+    public Task AddMessage_assembly()
+    {
+        var map = new MessageToHandlerMap();
+        map.AddMessagesFromAssembly<MyMessage>();
+        return Verify(map);
+    }
+
+    [Fact]
+    public Task AddHandler_type()
+    {
+        var map = new MessageToHandlerMap();
+        map.AddHandler<MyHandler>();
+        return Verify(map);
+    }
+
+    [Fact]
+    public Task AddHandler_assembly()
+    {
+        var map = new MessageToHandlerMap();
+        map.AddHandlersFromAssembly<MyHandler>();
+        return Verify(map);
+    }
+
+    Task Verify(MessageToHandlerMap map)
+    {
+        return Verify(new {map.Handlers, map.Messages});
+    }
+
+    public MessageToHandlerMapTests(ITestOutputHelper output) :
+        base(output)
+    {
+    }
+
+    class MyMessage : IMessage
+    {
+
+    }
+
+    class MessageWithNoHandler : IMessage
+    {
+
+    }
+
+    class MyHandler : IHandleMessages<MyMessage>
+    {
+        public Task Handle(MyMessage message, IMessageHandlerContext context)
+        {
+            return null!;
+        }
+    }
+}
---
+++ src/Verify.NServiceBus/Converters/MessageToHandlerMapConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Newtonsoft.Json;
+using Verify;
+using Verify.NServiceBus;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
+
+class MessageToHandlerMapConverter :
+    WriteOnlyJsonConverter<MessageToHandlerMap>
+{
+    public override void WriteJson(JsonWriter writer, MessageToHandlerMap? map, JsonSerializer serializer)
+    {
+        if (map == null)
+        {
+            return;
+        }
+
+        var messagesWithNoHandler = map.Messages.Except(map.Handlers.SelectMany(x => x.Value)).ToList();
+        if (!messagesWithNoHandler.Any())
+        {
+            return;
+        }
+
+        writer.WriteStartObject();
+        writer.WritePropertyName("MessagesWithNoHandler");
+        serializer.Serialize(writer, messagesWithNoHandler);
+        writer.WriteEndObject();
+    }
+}
