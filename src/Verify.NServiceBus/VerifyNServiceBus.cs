bc05259ce2a8dba456c227a9e494d2641cbc2168
Simon Cropp <simon.cropp@gmail.com>
2022-05-31 10:19:13 +1000

    release beta

--- src/Verify.NServiceBus/VerifyNServiceBus.cs
+++ src/Verify.NServiceBus/VerifyNServiceBus.cs
@@ -14,33 +14,31 @@ public static class VerifyNServiceBus
         {
             LogCapture.Initialize();
         }
-        VerifierSettings.ModifySerialization(settings =>
+
+        VerifierSettings.IgnoreMember<TestableMessageProcessingContext>(x => x.MessageHeaders);
+        VerifierSettings.IgnoreMember<TestableInvokeHandlerContext>(x => x.Headers);
+        VerifierSettings.IgnoreMember<TestableMessageProcessingContext>(x => x.MessageId);
+        VerifierSettings.IgnoreMember<TestableInvokeHandlerContext>(x => x.MessageHandler);
+        VerifierSettings.IgnoreMember<TestableInvokeHandlerContext>(x => x.MessageBeingHandled);
+        VerifierSettings.IgnoreMember<TestableInvokeHandlerContext>(x => x.MessageMetadata);
+        VerifierSettings.IgnoreMember<IMessageProcessingContext>(x => x.ReplyToAddress);
+        VerifierSettings.IgnoreMember<TestableEndpointInstance>(x => x.EndpointStopped);
+        VerifierSettings.IgnoreMember<TestableOutgoingLogicalMessageContext>(x => x.RoutingStrategies);
+        VerifierSettings.IgnoreMember<TestableOutgoingPhysicalMessageContext>(x => x.RoutingStrategies);
+        VerifierSettings.IgnoreMember<TestableRoutingContext>(x => x.RoutingStrategies);
+        VerifierSettings.IgnoreInstance<ContextBag>(x => !ContextBagHelper.HasContent(x));
+        VerifierSettings.IgnoreMembersWithType<IBuilder>();
+        VerifierSettings.AddExtraSettings(serializerSettings =>
         {
-            settings.IgnoreMember<TestableMessageProcessingContext>(x => x.MessageHeaders);
-            settings.IgnoreMember<TestableInvokeHandlerContext>(x => x.Headers);
-            settings.IgnoreMember<TestableMessageProcessingContext>(x => x.MessageId);
-            settings.IgnoreMember<TestableInvokeHandlerContext>(x => x.MessageHandler);
-            settings.IgnoreMember<TestableInvokeHandlerContext>(x => x.MessageBeingHandled);
-            settings.IgnoreMember<TestableInvokeHandlerContext>(x => x.MessageMetadata);
-            settings.IgnoreMember<IMessageProcessingContext>(x => x.ReplyToAddress);
-            settings.IgnoreMember<TestableEndpointInstance>(x => x.EndpointStopped);
-            settings.IgnoreMember<TestableOutgoingLogicalMessageContext>(x => x.RoutingStrategies);
-            settings.IgnoreMember<TestableOutgoingPhysicalMessageContext>(x => x.RoutingStrategies);
-            settings.IgnoreMember<TestableRoutingContext>(x => x.RoutingStrategies);
-            settings.IgnoreInstance<ContextBag>(x => !ContextBagHelper.HasContent(x));
-            settings.IgnoreMembersWithType<IBuilder>();
-            settings.AddExtraSettings(serializerSettings =>
-            {
-                var converters = serializerSettings.Converters;
-                converters.Add(new ContextBagConverter());
-                converters.Add(new SendOptionsConverter());
-                converters.Add(new ExtendableOptionsConverter());
-                converters.Add(new UnsubscriptionConverter());
-                converters.Add(new TimeoutMessageConverter());
-                converters.Add(new MessageToHandlerMapConverter());
-                converters.Add(new SubscriptionConverter());
-                converters.Add(new OutgoingMessageConverter());
-            });
+            var converters = serializerSettings.Converters;
+            converters.Add(new ContextBagConverter());
+            converters.Add(new SendOptionsConverter());
+            converters.Add(new ExtendableOptionsConverter());
+            converters.Add(new UnsubscriptionConverter());
+            converters.Add(new TimeoutMessageConverter());
+            converters.Add(new MessageToHandlerMapConverter());
+            converters.Add(new SubscriptionConverter());
+            converters.Add(new OutgoingMessageConverter());
         });
     }
 }
