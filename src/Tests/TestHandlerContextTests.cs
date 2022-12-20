c9db7b278771fa27431a6bcf69f49f8f54e8deb7
Simon <simon.cropp@gmail.com>
2022-12-20 11:41:51 +1100

    .

---
+++ src/Tests/TestHandlerContextTests.cs
@@ -0,0 +1,20 @@
+﻿[UsesVerify]
+public class TestHandlerContextTests
+{
+    [Fact]
+    public Task Simple()
+    {
+        var context = new TestHandlerContext();
+        context.Publish(new MyEvent());
+        context.Send(new MyCommand());
+        context.Reply(new MyCommand());
+        context.ForwardCurrentMessageTo("forward-destination");
+        context.Headers.Add("HeaderKey","Value");
+        context.MessageHeaders.Add("MessageHeaderKey","Value");
+        context.Extensions.Set("ExtensionKey","Value");
+        return Verify(context);
+    }
+
+    public record MyEvent;
+    public record MyCommand;
+}
