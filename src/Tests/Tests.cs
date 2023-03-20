a7e42a0c67093d723f8b8a1331d50ef28bc01b25
Simon <simon.cropp@gmail.com>
2023-03-20 15:23:29 +1100

    .

--- src/Tests/Tests.cs
+++ src/Tests/Tests.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.InteropServices.ComTypes;
-
-[UsesVerify]
+﻿[UsesVerify]
 public class Tests
 {
     [Fact]
--- src/Verify.NServiceBus/Converters/MessageHandlerContextConverter.cs
+++ src/Verify.NServiceBus/Converters/MessageHandlerContextConverter.cs
@@ -5,12 +5,12 @@ class MessageHandlerContextConverter :
     {
         writer.WriteStartObject();
         writer.WriteMember(context, context.RepliedMessages, "RepliedMessages");
-        writer.WriteMember(context, context.PublishedMessages, "PublishedMessages");
         writer.WriteMember(context, context.TimeoutMessages, "TimeoutMessages");
+        writer.WriteMember(context, context.ForwardedMessages, "ForwardedMessages");
         writer.WriteMember(context, context.SentMessages, "SentMessages");
         writer.WriteMember(context, context.Extensions, "Extensions");
-        writer.WriteMember(context, context.ForwardedMessages, "ForwardedMessages");
         writer.WriteMember(context, context.MessageHeaders, "MessageHeaders");
+        writer.WriteMember(context, context.PublishedMessages, "PublishedMessages");
         if (context.ReplyToAddress != "reply address")
         {
             writer.WriteMember(context, context.ReplyToAddress, "ReplyToAddress");
