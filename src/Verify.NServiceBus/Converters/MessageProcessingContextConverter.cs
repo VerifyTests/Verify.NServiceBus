ec071baf175e78590e4ba44fdf099f1bb766ddd3
Simon <simon.cropp@gmail.com>
2023-03-20 15:12:21 +1100

    Update MessageProcessingContextConverter.cs

--- src/Verify.NServiceBus/Converters/MessageProcessingContextConverter.cs
+++ src/Verify.NServiceBus/Converters/MessageProcessingContextConverter.cs
@@ -5,15 +5,14 @@ class MessageProcessingContextConverter :
     {
         writer.WriteStartObject();
 
-        writer.WriteMember(context, context.ForwardedMessages, "ForwardedMessages");
-        writer.WriteMember(context, context.MessageHeaders, "MessageHeaders");
         writer.WriteMember(context, context.RepliedMessages, "RepliedMessages");
+        writer.WriteMember(context, context.ForwardedMessages, "ForwardedMessages");
+        writer.WriteMember(context, context.SentMessages, "SentMessages");
         if (context.ReplyToAddress != "reply address")
         {
             writer.WriteMember(context, context.ReplyToAddress, "ReplyToAddress");
         }
         writer.WriteMember(context, context.PublishedMessages, "PublishedMessages");
-        writer.WriteMember(context, context.SentMessages, "SentMessages");
         writer.WriteMember(context, context.TimeoutMessages, "TimeoutMessages");
         writer.WriteMember(context, context.Extensions, "Extensions");
 
