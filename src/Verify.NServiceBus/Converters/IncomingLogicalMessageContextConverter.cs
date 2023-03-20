db46f89262e30bd7986f390ba90ec664a743da91
Simon <simon.cropp@gmail.com>
2023-03-20 15:19:35 +1100

    Update IncomingLogicalMessageContextConverter.cs

--- src/Verify.NServiceBus/Converters/IncomingLogicalMessageContextConverter.cs
+++ src/Verify.NServiceBus/Converters/IncomingLogicalMessageContextConverter.cs
@@ -7,8 +7,8 @@ class IncomingLogicalMessageContextConverter :
         writer.WriteMember(context, context.Message, "Message");
         writer.WriteMember(context, context.Headers, "Headers");
         writer.WriteMember(context, context.MessageHandled, "MessageHandled");
-        writer.WriteMember(context, context.PublishedMessages, "PublishedMessages");
         writer.WriteMember(context, context.SentMessages, "SentMessages");
+        writer.WriteMember(context, context.PublishedMessages, "PublishedMessages");
         writer.WriteMember(context, context.TimeoutMessages, "TimeoutMessages");
         writer.WriteMember(context, context.Extensions, "Extensions");
         writer.WriteMember(context, context.ForwardedMessages, "ForwardedMessages");
