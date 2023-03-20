ae0b4e1e4c3539a443e24a57972183b8e1f505c1
Simon <simon.cropp@gmail.com>
2023-03-21 08:48:04 +1100

    missing PublishedMessages

--- src/Verify.NServiceBus/Converters/MessageSessionConverter.cs
+++ src/Verify.NServiceBus/Converters/MessageSessionConverter.cs
@@ -7,6 +7,7 @@ class MessageSessionConverter :
 
         writer.WriteMember(context, context.TimeoutMessages, "TimeoutMessages");
         writer.WriteMember(context, context.SentMessages, "SentMessages");
+        writer.WriteMember(context, context.PublishedMessages, "PublishedMessages");
         writer.WriteMember(context, context.Subscriptions, "Subscriptions");
         writer.WriteMember(context, context.Unsubscription, "Unsubscriptions");
 
