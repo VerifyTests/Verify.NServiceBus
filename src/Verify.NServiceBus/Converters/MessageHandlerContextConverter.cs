65ecb34cfcb88f986d9080c4ca8a1a58dd1e65c6
Simon <simon.cropp@gmail.com>
2023-03-20 15:24:14 +1100

    Update MessageHandlerContextConverter.cs

--- src/Verify.NServiceBus/Converters/MessageHandlerContextConverter.cs
+++ src/Verify.NServiceBus/Converters/MessageHandlerContextConverter.cs
@@ -8,9 +8,9 @@ class MessageHandlerContextConverter :
         writer.WriteMember(context, context.TimeoutMessages, "TimeoutMessages");
         writer.WriteMember(context, context.ForwardedMessages, "ForwardedMessages");
         writer.WriteMember(context, context.SentMessages, "SentMessages");
-        writer.WriteMember(context, context.Extensions, "Extensions");
         writer.WriteMember(context, context.MessageHeaders, "MessageHeaders");
         writer.WriteMember(context, context.PublishedMessages, "PublishedMessages");
+        writer.WriteMember(context, context.Extensions, "Extensions");
         if (context.ReplyToAddress != "reply address")
         {
             writer.WriteMember(context, context.ReplyToAddress, "ReplyToAddress");
